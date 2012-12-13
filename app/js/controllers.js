f0a714234a145ffdcb808b0a0cf945b479b52fef
Jethro <jlarson@build.com>
2012-12-13 14:46:34 -0800

    Frist commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,31 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+function ProductController($scope,$http) {
+	
+
+	$http.get('/api/build-product-webservices-1.0.0/services/products?productId=k-690&manufacturer=kohler').success(function(data){
+		data = data.filter(function(finish){
+			return finish.status == 'stock';
+		});
+		var p = data[0],
+			finish;
+		p.finishes = [];
+
+		for(var i = 0,len=data.length;i<len;i++){
+			
+			finish = data[i];
+			p.finishes.push({
+				image: finish.image
+				,name: finish.finish
+				,cost: finish.cost
+			});
+		}
+		$scope.product = p;
+	});
+	$scope.dog = 'woof';
+}
+// ProductController.$inject = ['$scope,$http'];
 
 
 function MyCtrl2() {
