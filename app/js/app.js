252632f5496a432c75bb2ed85c4b415fe1c5e7d8
Jethro <jlarson@build.com>
2012-12-14 10:19:24 -0800

    Finish selector works, Some styling and boxing worked out in the main content.

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/product.html', controller: ProductController});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/product/:manufacturer/:productId/?:uid/?:section', {templateUrl: 'partials/product.html', controller: ProductController});
+    $routeProvider.otherwise({redirectTo: '/product/kohler/k-690'});
   }]);
 
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,25 +3,37 @@
 /* Controllers */
 
 
-function ProductController($scope,$http) {
+function ProductController($scope,$http, $routeParams) {
 	
-	var productId = 'k-690',
-		manufacturer = 'kohler';
-	$http.get('/api/build-product-webservices-1.0.0/services/products?productId='+productId+'&manufacturer='+manufacturer).success(function(data){
+	var productId = $routeParams.productId,
+		manufacturer = $routeParams.manufacturer;
+	$http.get('/api/build-product-webservices-1.0.0/services/products',{
+		params: {
+			productId: productId,
+			manufacturer: manufacturer
+		},
+		cache: true
+	}).success(function(data){
+		var p,finish;
 		data = data.filter(function(finish){
 			return finish.status == 'stock';
 		});
-		var p = data[0],
-			finish;
+		if($routeParams.uid){
+			p = data.filter(function(finish){
+				return finish.uniqueId == +$routeParams.uid;
+			})[0];
+		}else{
+			p = data[0];
+		}
 		p.finishes = [];
-
 		for(var i = 0,len=data.length;i<len;i++){
 			
 			finish = data[i];
 			p.finishes.push({
 				image: finish.image,
 				name: finish.finish,
-				cost: finish.cost
+				cost: finish.cost,
+				uid: finish.uniqueId
 			});
 		}
 		document.title = p.manufacturer+" "+ p.productId +" - "+ "Build.com";
